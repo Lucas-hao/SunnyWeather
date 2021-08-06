3860b3ff2218b9f05650e70d1513adda79090f59
Lucas-hao <843691596@qq.com>
2021-08-06 20:51:26 +0800

    1. 实现搜索全球城市数据功能<br>2. 加入显示天气信息的功能

---
+++ app/src/main/java/lucashao/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,19 @@
+package lucashao.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        const val TOKEN = "Gif1T8687f0JNd9f"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context : Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/lucashao/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -0,0 +1,28 @@
+package lucashao.sunnyweather.android.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import lucashao.sunnyweather.android.logic.Repository
+import lucashao.sunnyweather.android.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+
+    fun savePlace(place: Place) = Repository.savePlace(place)
+
+    fun getSavedPlace() = Repository.getSavedPlace()
+
+    fun isPlaceSaved() = Repository.isPlaceSaved()
+
+}
